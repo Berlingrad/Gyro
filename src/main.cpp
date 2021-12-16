7ee5c511d6f0b4ff8770c54918516e812f2621c8
Sam Xu <stormrisexu@hotmail.com>
2021-12-15 21:00:08 -0500

    PostProcess

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <mbed.h>
 #include "l3gd20_driver.h"
 #include "math.h"
+#define PI 3.1415926
 
 
 
@@ -8,7 +9,7 @@
 
 int main() {
     
-  /**
+     /**
     SPI spi(PF_9, PF_8, PF_7); // mosi, miso, sclk
     DigitalOut cs(PC_1);
     
@@ -30,7 +31,7 @@ int main() {
   
       // Send a dummy byte to receive the contents of the WHOAMI register
       int whoami = spi.write(0x00);
-      printf("WHOAMI register = %d\n", whoami);
+      printf("WHOAMI register = 0x%X\n", whoami);
   
       // Deselect the device
       cs = 1;
@@ -39,8 +40,12 @@ int main() {
       
     }
     **/
-
     
+    float degree2radian, degree, radian_angle, placement, radius, timer;
+    int counter;
+    float sum;
+    float array[40];
+   
     SPI spi(PF_9, PF_8, PF_7); // mosi, miso, sclk
     L3GD20Gyroscope gyroscope(&spi, PC_1);
     
@@ -51,20 +56,51 @@ int main() {
     }
 
     float w_dps[3];
-    int count = 0;
+    counter = 0;
+    radius = 0.4; 
+    sum = 0;
+    timer = 0;
+    
 
     while (1) {
+
+        
         
         gyroscope.read_data_dps(w_dps);
         float x = w_dps[0];
         float y = w_dps[1];
         float z = w_dps[2];
-        printf("wx: %+7.2f dps, wy: %+7.2f dps, wz: %+7.2f dps\n",  x, y, z);
+        //printf("wx: %+7.2f dps, wy: %+7.2f dps, wz: %+7.2f dps\n",  x, y, z);
 
+        degree2radian = PI / 180;
+        degree = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+        placement = radius * sin(degree * 0.0174533);
+        
+        array[counter] = (placement > 0.1) ? placement : 0;
+
+        sum += array[counter];
+
+        counter ++;
+        timer ++;
+
+        if (counter >= 39) {
+          counter = 0;
+        }
+
+        
+        
+        
+
+        printf("Time: %+7.1fs, wx: %+7.2f dps, wy: %+7.2f dps, wz: %+7.2f dps, cumulative distance:%+7.2fm, step length: %+7.2fm\n", timer * 0.5, x, y, z, sum, placement);
      
         ThisThread::sleep_for(500ms);
-        count++;
+ 
+    
+
+        
+
     }
+    
     
     
 }
